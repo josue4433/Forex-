79aee7a5786a42371566ee448a21703091313733
Josue Aguirre <113935346+josue4433@users.noreply.github.com>
2023-08-04 08:48:54 -0400

    second commit

--- app.py
+++ app.py
@@ -1,46 +1,19 @@
-# Core Library modules
-from typing import Dict, Optional, Union
-
-# Third party modules
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-# First party modules
-from mini_app import config
-
-db = SQLAlchemy()
+app = Flask(__name__)
 
 
-def create_app(cfg: Optional[config.Config] = None) -> Flask:
-    if cfg is None:
-        cfg = config.Config()
-    app = Flask(__name__, template_folder="templates")
-    app.config.from_object(cfg)
+@app.route('/', methods=['GET', 'POST'])
+def currency_converter():
+    if request.method == 'POST':
+        # Get user inputs from the form
+        from_currency = request.form['from_currency']
+        to_currency = request.form['to_currency']
+        amount = request.form['amount']
 
-    # Init_apps
-    db.init_app(app)
+       amount = float(amount)
+        except ValueError:
+            return render_template('errors.html', error_message='Invalid amount.')
 
-    @app.route("/square")
-    def square() -> str:
-        """A view which uses no templates and no database."""
-        number = int(request.args.get("number", 0))
-        return str(number ** 2)
+        
 
-    @app.route("/")
-    def templated_square() -> str:
-        """A view which uses templates."""
-        number = int(request.args.get("number", 0))
-        return render_template("base.html", number=number, square=number ** 2)
-
-    @app.route("/author/<int:author_id>")
-    def get_author(author_id: int) -> Dict[str, Union[int, str]]:
-        """A view which uses the database."""
-        from mini_app.models import Author
-
-        return Author.query.get(author_id).dict()
-
-    return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    return render_template('form.html')
